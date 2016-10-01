29112991b52966e0c98fc6ffeda30c57f84287f3
Davo Faulkner <d@davo.co>
2016-10-01 16:35:55 -0500

    Add SimpleFragmentPagerAdapter and Four Fragments (no errors)

---
+++ app/src/main/java/co/davo/nashvilletourguide/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,42 @@
+package co.davo.nashvilletourguide;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+/**
+ * Created by Davo on 10/1/2016.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+
+
+    private String tabTitles[] = new String[] { "Music" , "Food", "Art", "History" };
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new MusicFragment();
+        } else if (position == 1){
+            return new FoodFragment();
+        } else if (position == 2){
+            return new ArtFragment();
+        } else {
+            return new HistoryFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
