178a96dc2b90f164873273c599feceb2167bf0a1
Davo Faulkner <d@davo.co>
2016-10-04 00:22:57 -0500

    Add image resources for FoodFragment (no errors)

--- app/src/main/java/co/davo/nashvilletourguide/FoodFragment.java
+++ app/src/main/java/co/davo/nashvilletourguide/FoodFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +24,21 @@ public class FoodFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("Food Fragment");
-        return textView;
+        View rootView = inflater.inflate(R.layout.location_list, container, false);
+
+        final ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new Location("Hattie B's", "Hot Chicken", "101 Broad St\nNashville, TN 37201", R.drawable.hattiebs));
+        locations.add(new Location("Demos'", "Steak and Spaghetti", "101 Broad St\nNashville, TN 37201", R.drawable.demos));
+        locations.add(new Location("Grilled Cheeserie", "Hot Chicken", "101 Broad St\nNashville, TN 37201", R.drawable.grilledcheeserie));
+        locations.add(new Location("Loveless Cafe", "Hot Biscuits", "101 Broad St\nNashville, TN 37201", R.drawable.loveless));
+
+        LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
