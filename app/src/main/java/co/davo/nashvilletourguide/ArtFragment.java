ea1f61175179fa6b693f46cf8c9d1eb9d55e4ab3
Davo Faulkner <d@davo.co>
2016-10-04 17:15:49 -0500

    Add image resources to ArtFragment

--- app/src/main/java/co/davo/nashvilletourguide/ArtFragment.java
+++ app/src/main/java/co/davo/nashvilletourguide/ArtFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +25,21 @@ public class ArtFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText("Art Fragment");
-        return textView;
+        View rootView = inflater.inflate(R.layout.location_list, container, false);
+
+        final ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new Location("Grand Ole Opry", "Music and Dancing", "101 Broad St\nNashville, TN 37201", R.drawable.parthenon));
+        locations.add(new Location("Grand Ole Opry", "Music and Dancing", "101 Broad St\nNashville, TN 37201", R.drawable.parthenon_int));
+        locations.add(new Location("Grand Ole Opry", "Music and Dancing", "101 Broad St\nNashville, TN 37201", R.drawable.frist));
+        locations.add(new Location("Grand Ole Opry", "Music and Dancing", "101 Broad St\nNashville, TN 37201", R.drawable.cheekwood));
+
+        LocationAdapter adapter = new LocationAdapter(getActivity(), locations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
